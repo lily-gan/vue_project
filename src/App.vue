d9b1fcca284945d2a9dff1682222bff0f9c1d420
gyl <gyl@qq.com>
2019-04-24 13:48:52 +0800

    初始化静态页+基本路由+轮播图+插槽

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <router-view/>
+    <FootGuide v-show="$route.meta.isShowFooter"/>
   </div>
 </template>
 
 <script>
+import FootGuide from './components/FootGuide/FootGuide.vue'
 export default {
   name: 'App',
   components: {
+    FootGuide
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router/router'
+import Header from '../src/components/Header/Header.vue'
+Vue.component('Header', Header)
 
 Vue.config.productionTip = false
 
@@ -9,5 +12,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router
 })
